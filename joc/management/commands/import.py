2051644326b9bb28ee11f966573631ff948edbb6
flackdl <flackattack@gmail.com>
2019-12-29 08:37:13 -0600

    parse toc

--- joc/management/commands/import.py
+++ joc/management/commands/import.py
@@ -1,4 +1,5 @@
 import os
+import xml.etree.ElementTree as ET
 from django.core.management.base import BaseCommand, CommandError
 
 
@@ -13,3 +14,9 @@ class Command(BaseCommand):
         if not os.path.exists(epub):
             raise CommandError('epub "%s" does not exist' % epub)
         self.stdout.write(self.style.SUCCESS('Processing "%s"' % epub))
+        tree = ET.parse('/home/danny/Dropbox/joc/toc.ncx')
+        root = tree.getroot()
+        ns = {'ncx': 'http://www.daisy.org/z3986/2005/ncx/'}
+        nav_map_el = root.find('ncx:navMap', ns)
+        for chapter in nav_map_el.findall('ncx:navPoint', ns):
+            self.stdout.write(self.style.SUCCESS(chapter))
