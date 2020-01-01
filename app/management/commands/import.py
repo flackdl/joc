2e8f34d85c16901354e832d175a635383c3af066
flackdl <flackattack@gmail.com>
2020-01-01 16:13:27 -0600

    category descriptions

--- app/management/commands/import.py
+++ app/management/commands/import.py
@@ -28,36 +28,50 @@ class Command(BaseCommand):
     def process_toc_item(self, el: ET.Element, parent=None):
         el_title = el.find('ncx:navLabel/ncx:text', self.ns).text
         children = el.findall('ncx:navPoint', self.ns)
+        content = el.find('ncx:content', self.ns)
+        # separate content html path and content fragment id
+        content_html, content_id = re.match(r'^(.*\.html)(?:#?)(.*)$', content.attrib['src']).groups()
+        content_path = os.path.join(self.path, content_html)
+        tree = lxml.html.parse(content_path)
+        root = tree.getroot()
 
         # child
-        if not children and parent:
-            content = el.find('ncx:content', self.ns)
-            content_html = re.sub(r'#.*', '', content.attrib['src'])  # remove fragment
-            content_path = os.path.join(self.path, content_html)
-            tree = lxml.html.parse(content_path)
-            root = tree.getroot()
-            for recipe in root.findall('.//h3'):
-                self.stdout.write(self.style.SUCCESS('Adding recipe {} to {}'.format(recipe.text_content(), parent)))
-                instructions = []
-                instruction_el = recipe.getnext()
-                while True:
-                    if instruction_el is None:
-                        break
-                    instructions.append(instruction_el.text_content())
-                    instruction_el = instruction_el.getnext()
-                Recipe.objects.get_or_create(
-                    name=recipe.text_content(),
-                    defaults=dict(
-                        instructions='\n'.join(instructions),
-                        category=parent,
-                    )
+        if content_id and not children and parent:
+            recipe_el = root.find(".//h3[@id='{}']".format(content_id))
+            self.stdout.write(self.style.SUCCESS('Adding recipe {} to {}'.format(recipe_el.text_content(), parent)))
+            instruction_el = recipe_el.getnext()
+            instructions = []
+            # assume every sibling is part of the ingredients until you run hit the end or another recipe
+            while True:
+                # end of recipe
+                if instruction_el is None or instruction_el.tag == 'h3':
+                    break
+                instructions.append(instruction_el.text_content())
+                instruction_el = instruction_el.getnext()
+            Recipe.objects.get_or_create(
+                name=recipe_el.text_content().lower(),
+                defaults=dict(
+                    instructions='\n'.join(instructions),
+                    category=parent,
                 )
+            )
         # category
         else:
             self.stdout.write(self.style.SUCCESS('Adding category {}'.format(el_title)))
+            category_el = root.find(".//h2")
+            descriptions = []
+            if category_el is not None:
+                category_el = category_el.getnext()
+                while True:
+                    # end of category
+                    if category_el is None or category_el.tag in ['h2', 'h3']:
+                        break
+                    descriptions.append(category_el.text_content())
+                    category_el = category_el.getnext()
             category, was_created = Category.objects.get_or_create(
-                name=el_title,
+                name=el_title.lower(),
                 defaults=dict(
+                    description='\n'.join(descriptions),
                     parent=parent,
                 ),
             )
--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=500, unique=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    description = models.TextField()
 
     def __str__(self):
         return self.name
